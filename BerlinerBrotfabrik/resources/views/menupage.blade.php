59335883b9c3ff3e574bf52e2930c2b6c941c831
Jasu10 <143518984+Jasu10@users.noreply.github.com>
2024-03-24 23:25:35 +0800

    Changes

--- BerlinerBrotfabrik/resources/views/menupage.blade.php
+++ BerlinerBrotfabrik/resources/views/menupage.blade.php
@@ -16,24 +16,23 @@
 <body style="padding: 0; margin: 0;">
     @include('layouts.navbar')
    <!-- Best Seller Carousel -->
-   <div class="container-fluid position-relative">
-    <div class="row align-items-center">
-        <!-- Image -->
-    <div class="col-md-6 p-0" style="width: 500px; height: 500px; overflow: hidden;">
+   
+        <div class="relative flex flex-col sm:flex-row items-stretch h-[500px]">
+    <!-- Image -->
+    <div class="col-md-6 p-0" style="width: 100%; height: 500px; overflow: hidden;">
         @foreach ($bestSellerItems as $key => $item)
-        <div class="carousel-item {{ $key == 0 ? 'active' : '' }}">
-            <img id="carouselImage" class="img-fluid w-100 h-100" src="{{ asset('images/' . $item->image) }}" alt="{{ $item->name }}" style="object-fit: cover;">
+        <div class="carousel-item {{ $key == 0 ? 'active' : '' }}" style="height: 100%;">
+            <img id="carouselImage" class="img-fluid w-100 h-100" src="{{ asset('images/' . $item->image) }}" alt="{{ $item->name }}" style="object-fit: cover; object-position: center;">
         </div>
         @endforeach
     </div>
     <!-- Description Box -->
-    <div class="col-md-6" style="background-color: #789461; width: 500px; height: 500px; overflow: auto;">
+    <div class="col-md-6" style="background-color: #789461; width: 100%; height: 500px; overflow: auto;">
         <img class="best-seller-logo" src="{{ asset('img/best seller logo.png') }}" alt="Best Seller Logo">
         <h2 id="carouselTitle" class="item-name">{{ $bestSellerItems->first() ? $bestSellerItems->first()->name : 'Empty' }}</h2>
         <hr class="item-line" style="border: none; border-top: 3px solid #45474B; width: 80%; margin: 1rem auto;">
         <p id="carouselDescription" class="item-description" style="color: #DBE7C9; font-size: 20px; padding-left: 2rem; max-width: 80%; margin: 0 auto;">{{ $bestSellerItems->first() ? $bestSellerItems->first()->description : 'Empty' }}</p>
     </div>
-</div>
     <!-- Next Button -->
     <div class="position-absolute w-100 d-flex justify-content-center" style="top: 50%; right:0%;">
         <button class="next-button" onclick="nextSlide()">Next</button>
@@ -93,7 +92,7 @@ function getItemsByCategory(category) {
             // Add each item to the container
             items.forEach(item => {
                 let div = document.createElement('div');
-                div.className = 'max-w-xs rounded overflow-hidden shadow-md bg-white';
+                div.className = 'max-w-xs rounded overflow-hidden shadow-md bg-white fadeInUp';
                         
                 // Add the item image, name, and description to the div
                 div.innerHTML = `
